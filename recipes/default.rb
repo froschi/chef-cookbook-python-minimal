176beb8d3067cc59bf8d0b611ea5227458dc132d
Thorsten Fischer <thorsten@froschi.org>
2012-06-19 15:56:01 +0200

    Initial import.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

---
+++ recipes/default.rb
@@ -0,0 +1,18 @@
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid"
+  packages |= %w/
+    python2.6-minimal
+  /
+when "precise"
+  packages |= %w/
+    python2.6-minimal
+  /
+end
+
+packages.each do |pkg|
+  package pkg do
+    action [:install, :upgrade]
+  end
+end
